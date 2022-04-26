922d610772769bb13a62b889a68abaa05111a8ea
jonathan <bayart.jonathan@laposte.net>
2022-04-26 09:58:28 +0200

    mise en place du recaptchav3

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,10 +8,15 @@ use App\Entity\AccueilEvenement;
 use App\Entity\AccueilFamille;
 use App\Entity\AccueilMariage;
 use App\Entity\Carrousel;
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 
 class AccueilController extends AbstractController
 {
@@ -23,7 +28,7 @@ class AccueilController extends AbstractController
     }
 
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $photosCarrousel = $this->entityManager->getRepository(Carrousel::class)->findAll();
         $photosAccueilMariage = $this->entityManager->getRepository(AccueilMariage::class)->findByIsValid(1);
@@ -32,6 +37,36 @@ class AccueilController extends AbstractController
         $photosAccueilEnfant = $this->entityManager->getRepository(AccueilEnfant::class)->findByIsValid(1);
         $photosAccueilAnimaux = $this->entityManager->getRepository(AccueilAnimaux::class)->findByIsValid(1);
 
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            
+            $this->entityManager->persist($contact);
+            $this->entityManager->flush();
+
+            // envoi de la demande de formulaire par mail
+
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('admin@example.com')
+            ->subject('Demande de renseignements')
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context([
+                'contact' => $contact
+            ]);
+
+        $mailer->send($email);
+
+            $this->addFlash('messageEnvoye', 'Votre demande à etait envoyée avec succès!');
+            
+            return $this->redirectToRoute('app_accueil');
+        };
+
+
         
         return $this->render('accueil/accueil.html.twig', [
             'photosCarrousel' => $photosCarrousel,
@@ -39,7 +74,8 @@ class AccueilController extends AbstractController
             'photosAccueilFamille' => $photosAccueilFamille,
             'photosAccueilEvenement' => $photosAccueilEvenement,
             'photosAccueilEnfant' => $photosAccueilEnfant,
-            'photosAccueilAnimaux' => $photosAccueilAnimaux
+            'photosAccueilAnimaux' => $photosAccueilAnimaux,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomPrenom', TextType::class, [
+                'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
+                'attr' => [
+                    'placeholder' => 'Votre nom et prénom',
+                    'class' => 'inputContact'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 60
+                ]),
+                'attr' => [
+                    'placeholder' => 'Votre email',
+                    'class' => 'inputContact'
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 20
+                ]),
+                'attr' => [
+                    'placeholder' => 'Votre téléphone',
+                    'class' => 'inputContact'
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Une question? une demande de renseignements? c\'est par ici...',
+                    'class' => 'inputContact'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'btnForm'
+                ]
+            ])
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
